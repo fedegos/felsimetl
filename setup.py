b3fc46c91ec1b719981c854849d72f178a88566a
Federico Gosman <federico@equipogsm.com>
2018-05-03 19:07:23 -0300

    Created felsim package and added wheel

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='felsimetl',
-      version='0.1.2',
+      version='0.1.4',
       description='ETL for Felsim',
       url='http://github.com/fedegos/felsimetl',
       author='Federico Gosman',
